678ef6f24cddcf1e977e5be2fe5122337791966d
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-03-18 08:57:48 +0530

    Added Edit and trash icon on each list via javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,6 @@ const ls = window.localStorage
 const lists = ls.getItem('todos') ? JSON.parse(ls.getItem('todos')) : []
 let selectedList = []
 
-function createList (listName) {
-  return { id: Date.now().toString(), name: listName, tasks: [] }
-}
-
-function createTask (taskName) {
-  return { id: Date.now().toString(), name: taskName }
-}
-
 const addNewList = listName => {
   if (!listName) return
   const newList = createList(listName)
@@ -22,13 +14,34 @@ const addNewList = listName => {
   ls.setItem('todos', JSON.stringify(lists))
 }
 
+// const updateList = (element, id) => {
+//   console.log(element, id)
+//   console.log('vineet')
+// }
+
 const renderLists = lists => {
   lists.forEach(list => {
     const divElement = document.createElement('div')
-    divElement.appendChild(document.createTextNode(list.name))
-    // divElement.dataset.listId = list.id
+    const span = document.createElement('span')
+    span.textContent = list.name
+    divElement.id = list.id
+    divElement.appendChild(span)
+    const span1 = document.createElement('span')
+    const span2 = document.createElement('span')
+    const editIcon = '<i style="float:right; padding-right:5px" class="fa fa-pencil-square-o" aria-hidden="true"></i>'
+    const trashIcon = '<i style="float:right" class="fa fa-trash" aria-hidden="true"></i>'
+    divElement.appendChild(span2)
+    divElement.appendChild(span1)
+    span1.innerHTML = editIcon
+    span2.innerHTML = trashIcon
+    // document.getElementById('editIcon').setAttribute('onclick', console.log('edit'))
+    // document.getElementById('trashIcon').setAttribute('onclick', console.log('delete'))
+    span.setAttribute('onclick', 'selectedListRenderOnClick(' + span.parentElement.id + ')')
+    span1.setAttribute('onclick', 'selectedListEditOnClick(' + span.parentElement.id + ')')
+    span2.setAttribute('onclick', 'selectedListDeleteOnClick(' + span.parentElement.id + ')')
     divElement.id = list.id
     showListContainer.appendChild(divElement)
+    // divElement.setAttribute('ondblclik', updateList)
   })
 }
 
@@ -43,16 +56,30 @@ addListInput.addEventListener('keyup', function (event) {
   }
 })
 
-showListContainer.addEventListener('click', function (event) {
-  event.target.classList.toggle('active-list')
-  // console.log(event.target.id)
-  // console.log(event.target.getAttribute(['data-list-id']))
+const selectedListRenderOnClick = id => {
   reset(document.getElementById('lists-container'))
-
-  selectedList = lists.filter(list => list.id === event.target.id)[0]
-  // console.log(selectedList)
+  selectedList = lists.filter(list => list.id === String(id))[0]
   renderTasks(selectedList)
-})
+}
+
+const selectedListEditOnClick = id => {
+  console.log(id)
+  // reset(document.getElementById('lists-container'))
+  selectedList = lists.filter(list => list.id === String(id))[0]
+  const value = selectedList.value
+  console.log(value)
+  // renderTasks(selectedList)
+}
+// showListContainer.addEventListener('click', function (event) {
+//   event.target.classList.toggle('active-list')
+//   // console.log(event.target.id)
+//   // console.log(event.target.getAttribute(['data-list-id']))
+//   reset(document.getElementById('lists-container'))
+
+//   selectedList = lists.filter(list => list.id === event.target.id)[0]
+//   // console.log(selectedList)
+//   renderTasks(selectedList)
+// })
 
 const addNewTask = taskName => {
   if (!taskName) return
@@ -92,6 +119,14 @@ const reset = element => {
   while (element.firstChild) {
     element.removeChild(element.firstChild)
   }
+}
+
+function createList (listName) {
+  return { id: Date.now().toString(), name: listName, tasks: [] }
+}
+
+function createTask (taskName) {
+  return { id: Date.now().toString(), name: taskName }
 }
 
 const load = () => renderLists(lists)
